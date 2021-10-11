9c1f1897d7a71f4bacdd37baafc075e0f8f79e8f
JungleZ <1791079951@qq.com>
2021-10-11 11:25:25 +0800

    Update runTasks.sh

--- runTasks.sh
+++ runTasks.sh
@@ -1,9 +1,9 @@
 
 
-echo '1: 任务列表'
-echo '2: 添加任务'
-echo '3: 修改任务'
-echo "4: 删除任务"
+echo '1: USERS LIST'
+echo '2: ADD USERS'
+echo '3: UPDATE USERS'
+echo "4: DELETE USERS"
 
 echo '请输入选项:'
 
@@ -11,22 +11,31 @@ read type
 
 if [ $type -eq "1" ]
  then
-  echo "1"
   curl http://localhost:3000/users
- elif  [ $type -eq 2 ]
+    echo "show  user list is ok"
+ elif  [ $type -eq "2" ]
   then
+   echo "Please input new user name"
    read name
-   read pass
-   read avator
+   pass = "test"
+   avator = "true"
 
    curl -d'name=$name&pass=$pass&avator=$avator'-X POST http://localhost:3000/users/add
-   echo "2"
- elif  [ $type -eq 3 ] 
+   echo "insert $name is ok"
+ elif  [ $type -eq "3" ] 
   then
-   echo "3"
- elif  [ $type -eq 4 ] 
+   echo "Please input user ID:"
+   read id
+   echo "Please input new user name :"
+   read name
+   curl -d'name=$name&id=$id'-X PUT http://localhost:3000/users/updateById
+   echo "update $name is ok"
+ elif  [ $type -eq "4" ] 
   then
-   echo "4"
+  echo "Please input user ID:"
+   read id
+   curl -d'name=$name&id=$id'-X DELETE http://localhost:3000/users/delete/id
+   echo "deleted $name is ok"
  else
    echo "illegal argument"
  fi
