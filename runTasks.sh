3f55318c614a296a5d7eb32fb35346b87768369f
JungleZ <1791079951@qq.com>
2021-10-11 12:44:45 +0800

    Update runTasks.sh

--- runTasks.sh
+++ runTasks.sh
@@ -11,7 +11,7 @@ read type
 
 if [ $type -eq "1" ]
  then
-  curl http://localhost:3000/users
+  curl http://localhost:3000/
     echo "show  user list is ok"
  elif  [ $type -eq "2" ]
   then
@@ -20,7 +20,7 @@ if [ $type -eq "1" ]
    pass = "test"
    avator = "true"
 
-   curl -d'name=$name&pass=$pass&avator=$avator'-X POST http://localhost:3000/users/add
+   curl -d'name=$name&pass=$pass&avator=$avator'-X POST http://localhost:3000/users/
    echo "insert $name is ok"
  elif  [ $type -eq "3" ] 
   then
@@ -28,13 +28,13 @@ if [ $type -eq "1" ]
    read id
    echo "Please input new user name :"
    read name
-   curl -d'name=$name&id=$id'-X PUT http://localhost:3000/users/updateById
+   curl -d'name=$name&id=$id'-X PUT http://localhost:3000/users/
    echo "update $name is ok"
  elif  [ $type -eq "4" ] 
   then
   echo "Please input user ID:"
    read id
-   curl -d'name=$name&id=$id'-X DELETE http://localhost:3000/users/delete/id
+   curl -d'name=$name&id=$id'-X DELETE http://localhost:3000/users/
    echo "deleted $name is ok"
  else
    echo "illegal argument"
